f0ad35ee4a2b1bc2442b8ee9f350e925e1b31ef8
shwetal14 <95999701+shwetal14@users.noreply.github.com>
2021-12-30 00:06:35 +0530

    Add files via upload

---
+++ sample 2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+list = [8, 2, 3, 0, 7]
+a = 0
+
+for i in list:
+    a += i
+
+print(a)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
